ede0d3c5b1d00f9d074e35101a4d6c22744c7af7
ehddnr177 <ehddnr177@naver.com>
2022-01-14 13:39:00 +0900

    # 변경사항 1. 변수명 수정

--- framework/src/main/java/network/rtcp/type/extended/feedback/base/RtcpFeedbackControlInformationType.java
+++ framework/src/main/java/network/rtcp/type/extended/feedback/base/RtcpFeedbackControlInformationType.java
@@ -3,8 +3,8 @@ package network.rtcp.type.extended.feedback.base;
 public class RtcpFeedbackControlInformationType {
 
     public static final byte PAUSE = 0;
-    public static final byte RESUEM = 1;
-    public static final byte PASUED = 2;
+    public static final byte RESUME = 1;
+    public static final byte PAUSED = 2;
     public static final byte REFUSED = 3;
 
 }
